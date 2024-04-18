ef0b6b1497ed36c679535be9b71cabcc3b2cb9e5
JuraciMaia <152092131+JuraciMaia@users.noreply.github.com>
2024-04-17 22:31:35 -0300

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,22 @@
+
+ 
+#include <stdio.h>
+
+int main() {
+int n1, n2, m;
+    scanf("%d", &n1);
+    scanf("%d", &n2);
+    m = n1 + n2 ;
+    //printf("%d", m);
+    printf("soma = %d\n", m);
+    
+    return 0;
+}
+
+    
+    
+    
+    
+    
+    
+    
